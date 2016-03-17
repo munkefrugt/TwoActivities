f25dd38c60510df483c15c852a5c6c0005b6991b
munkefrugt@gmail.com <munkefrugt@gmail.com>
2016-03-18 00:14:16 +0100

    Så er den færdig.

--- app/src/main/java/com/example/twoactivities/DisplayMessageActivity.java
+++ app/src/main/java/com/example/twoactivities/DisplayMessageActivity.java
@@ -1,5 +1,6 @@
 package com.example.twoactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -31,8 +34,31 @@ public class DisplayMessageActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        // I added this one.
+
+
+        // FOLLOWING ADDED BY ME USING THE GUIDE:
+        // I added this Log message not needed .
         Log.i("Hi", "onCreate: New activity ");
+
+        // here we get the intent
+        Intent intent = getIntent(); // maybe this should be made on class level?
+
+        // Extract the message delivered by MainActivity with the getStringExtra() method.
+        // I changed in the exmple on the android site the MainActivity is called MyActivity.
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // we add an textView object
+        TextView textView = new TextView(this);
+
+        // set size and set the text, "message" is  the name of the String we got from the intent.
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        //Add the TextView to the RelativeLayout identified by R.id.content.
+        RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+        layout.addView(textView);
+
+
     }
 
 }
